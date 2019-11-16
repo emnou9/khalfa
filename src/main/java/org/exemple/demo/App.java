c695d7db5ae81c54a3c6cdb0527e4600402a29ef
emnou9 <36750616+emnou9@users.noreply.github.com>
2019-11-16 11:47:31 +0100

    Update App.java

--- src/main/java/org/exemple/demo/App.java
+++ src/main/java/org/exemple/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! hola" );
+        System.out.println( "Hello World! holan" );
     }
 }
